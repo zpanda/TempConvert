d5cb5d86c82b2cf82004d647036018d1f11a9a51
Pradyut Panda <ppanda@zynga.com>
2013-10-03 18:37:35 -0700

    Initial Commit

---
+++ TempConvert/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TempConvert
+//
+//  Created by ppanda on 10/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "xyzAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([xyzAppDelegate class]));
+    }
+}
---
+++ TempConvert/xyzAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  xyzAppDelegate.h
+//  TempConvert
+//
+//  Created by ppanda on 10/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface xyzAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
